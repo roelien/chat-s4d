40459ab4ac05226edf996bd2ca5a711eec31eff3
roelien <roelientippe@gmail.com>
2016-11-08 14:58:33 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require("express"),
+    app = express(),
+    server = require("http").createServer(app),
+    io = require("socket.io").listen(server);
+
+server.listen(3000);
+app.get("/", function (req, res) {
+    res.sendfile(__dirname + "/index.html");
+});
+
+io.sockets.on("connection", function (socket) {
+
+    // Sends a message every 1000 milliseconds
+    setInterval(function () {
+        var d = new Date();
+        io.sockets.emit('heartbeat', {hello: 'world!', date: d});
+    }, 1000);
+
+    // Sends a message to the chat board
+    socket.on("sendMessage", function (data) {
+        io.sockets.emit("newMessage", data);
+    });
+
+});
